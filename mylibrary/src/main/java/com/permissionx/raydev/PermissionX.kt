fa0f205c5b758f0f0de5061ac80b5a3e10016ef8
liuruilong <liuruilong@baidu.com>
2023-04-08 16:46:49 +0800

    commit permissionx library code

---
+++ mylibrary/src/main/java/com/permissionx/raydev/PermissionX.kt
@@ -0,0 +1,19 @@
+package com.permissionx.raydev
+
+import androidx.fragment.app.FragmentActivity
+
+object PermissionX {
+    private const val TAG = "InvisibleFragment"
+    fun request(activity: FragmentActivity, vararg permission: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        var fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permission)
+    }
+}
